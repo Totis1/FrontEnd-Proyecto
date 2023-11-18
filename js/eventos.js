7c018b12cec51f763b142586b2833f006d78b4cc
Franciscojmg22 <fdj.martinez@outlook.es>
2023-11-18 12:24:26 -0600

    slider ventas

---
+++ js/eventos.js
@@ -0,0 +1,15 @@
+const ecard = document.getElementById('eCard').content
+const epr = document.getElementById('pr')
+const econtainer = document.getElementById('eContainer')
+const fragment = document.createDocumentFragment()
+
+window.addEventListener('DOMContentLoaded', () => {
+    const nE = 8;
+    econtainer.innerHTML = ''
+    for (let index = 0; index < nE; index++) {
+        const clone = ecard.cloneNode(true)
+        fragment.appendChild(clone)
+        econtainer.appendChild(fragment)
+        
+    }
+})
