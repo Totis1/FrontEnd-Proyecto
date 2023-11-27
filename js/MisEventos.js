feba3178fe177d9586ff7e747778f9502c1a394a
Fabian Alexis <segoviano.balandran.fabian.1fm@gmail.com>
2023-11-27 10:26:16 -0600

    ProximosE/MisEventos

---
+++ js/MisEventos.js
@@ -0,0 +1,81 @@
+let mesActual = new Date().getMonth(); // 0-11
+let año = 2023; 
+
+document.getElementById('mesAnterior').addEventListener('click', function() {
+    cambiarMes(-1);
+});
+
+document.getElementById('mesSiguiente').addEventListener('click', function() {
+    cambiarMes(1);
+});
+
+function cambiarMes(cambio) {
+    mesActual += cambio;
+    if (mesActual < 0) {
+        mesActual = 11;
+    } else if (mesActual > 11) {
+        mesActual = 0;
+    }
+    generarCalendario(mesActual, año);
+}
+
+function generarCalendario(mes, año) {
+    const diasMes = new Date(año, mes + 1, 0).getDate();
+    const primerDiaMes = new Date(año, mes, 1).getDay();
+
+    let dias = [];
+    for (let i = 0; i < primerDiaMes; i++) {
+        dias.push("<div class='dia'></div>");
+    }
+
+    for (let dia = 1; dia <= diasMes; dia++) {
+        let fecha = `${año}-${String(mes + 1).padStart(2, '0')}-${String(dia).padStart(2, '0')}`;
+        dias.push(`<div class='dia' id='dia-${fecha}'>${dia}</div>`);
+    }
+
+    document.getElementById('dias').innerHTML = dias.join("");
+    document.getElementById('tituloMes').innerText = `${obtenerNombreMes(mes)} ${año}`;
+}
+
+function obtenerNombreMes(mes) {
+    const nombresMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    return nombresMeses[mes];
+}
+
+// Mostrar Ventanas Modales
+document.getElementById('btnAgregarEvento').addEventListener('click', function() {
+    document.getElementById('formularioAgregarEvento').showModal();
+});
+
+document.getElementById('btnModificarEvento').addEventListener('click', function() {
+    document.getElementById('formularioModificarEvento').showModal();
+});
+
+document.getElementById('btnEliminarEvento').addEventListener('click', function() {
+    document.getElementById('formularioEliminarEvento').showModal();
+});
+
+// Guardar evento
+document.getElementById('guardarEvento').addEventListener('click', function() {
+    var nombreEvento = document.getElementById('nombreEvento').value;
+    var fechaEvento = document.getElementById('fechaEventoAgregar').value;
+
+    if (nombreEvento && fechaEvento) {
+        var idDia = 'dia-' + fechaEvento;
+        var diaCalendario = document.getElementById(idDia);
+        
+        if (diaCalendario) {
+            var eventoDiv = document.createElement('div');
+            eventoDiv.className = 'nombreEvento';
+            eventoDiv.textContent = nombreEvento;
+            diaCalendario.appendChild(eventoDiv);
+        }
+    }
+});
+
+generarCalendario(mesActual, año);
+
+
+
+
+
--- js/eventos.js
+++ js/eventos.js
@@ -1,15 +1,97 @@
-const ecard = document.getElementById('eCard').content
 const epr = document.getElementById('pr')
-const econtainer = document.getElementById('eContainer')
-const fragment = document.createDocumentFragment()
+const ecard = document.getElementById('eCard').content;
+const econtainer = document.getElementById('eContainer');
+const mesTitulo = document.getElementById('mesTitulo');
+const nombreMes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+let mesActual = 11;
+const totalMeses = 12;
+
+const actualizarTarjetasYTitulo = () => {
+    document.querySelectorAll('.eve-Card').forEach(card => card.style.display = 'none');
+    document.querySelectorAll(`.eve-Card[data-mes="${mesActual}"]`).forEach(card => card.style.display = 'block');
+    mesTitulo.textContent = `${nombreMes[mesActual]} 2023`;
+};
+
+document.getElementById('btnRetroceder').addEventListener('click', () => {
+    mesActual = (mesActual - 1 + totalMeses) % totalMeses;
+    actualizarTarjetasYTitulo();
+});
+document.getElementById('btnAvanzar').addEventListener('click', () => {
+    mesActual = (mesActual + 1) % totalMeses;
+    actualizarTarjetasYTitulo();
+});
 
 window.addEventListener('DOMContentLoaded', () => {
-    const nE = 8;
-    econtainer.innerHTML = ''
-    for (let index = 0; index < nE; index++) {
-        const clone = ecard.cloneNode(true)
-        fragment.appendChild(clone)
-        econtainer.appendChild(fragment)
-        
+    const nE = 9;
+    econtainer.innerHTML = '';
+    const fragment = document.createDocumentFragment();
+
+    for (let mes = 0; mes < totalMeses; mes++) {
+        for (let index = 0; index < nE; index++) {
+            const clone = ecard.cloneNode(true);
+            clone.querySelector('.eve-Card').setAttribute('data-mes', mes);
+            fragment.appendChild(clone);
+        }
     }
-})
+    econtainer.appendChild(fragment);
+
+    actualizarTarjetasYTitulo();
+});
+
+// ---- Slide ----
+let index = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+let slideInterval;
+
+function changeSlide(step) {
+    slides[index].classList.remove('active');
+    index = (index + step + totalSlides) % totalSlides;
+    slides[index].classList.add('active');
+}
+
+function startSlideShow() {
+    slideInterval = setInterval(() => {
+        changeSlide(1);
+    }, 3000); 
+}
+
+function stopSlideShow() {
+    clearInterval(slideInterval);
+}
+
+// Funciones para cambiar el tamaño de la información del slide
+function reduceSlideInfo() {
+    let activeSlide = document.querySelector('.slide.active');
+    let infoPart = activeSlide.querySelector('.slide-info');
+
+    infoPart.style.width = '50%';
+}
+
+function resetSlideInfoSize() {
+    let activeSlide = document.querySelector('.slide.active');
+    let infoPart = activeSlide.querySelector('.slide-info');
+
+    infoPart.style.width = '70%';
+}
+
+// Eventos de mouseover y mouseout en el carrusel
+const sliderContainer = document.getElementById('slider-container');
+sliderContainer.addEventListener('mouseover', function() {
+    stopSlideShow();
+    reduceSlideInfo();
+});
+sliderContainer.addEventListener('mouseout', function() {
+    startSlideShow();
+    resetSlideInfoSize();
+});
+
+slides[index].classList.add('active');
+startSlideShow();
+
+
+
+
+
+
