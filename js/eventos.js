48416ff299fbaf5eddef7954f308d630adecae89
apocalix <lololertrololer@gmail.com>
2023-12-07 00:51:19 -0600

    feat: miseventos and eventos added
    calendar, slider to show events and modify edit add delete buttons
    
    Co-Authored-By: Fabian Alexis <143432964+FabianAl3xis@users.noreply.github.com>

--- js/eventos.js
+++ js/eventos.js
@@ -2,6 +2,91 @@ const ecard = document.getElementById("eCard").content;
 const epr = document.getElementById("pr");
 const econtainer = document.getElementById("eContainer");
 const fragment = document.createDocumentFragment();
+const mesTitulo = document.getElementById("mesTitulo");
+const nombreMes = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+];
+
+let mesActual = 11;
+const totalMeses = 12;
+
+const actualizarTarjetasYTitulo = () => {
+    document.querySelectorAll(".eve-Card").forEach((card) => (card.style.display = "none"));
+    document.querySelectorAll(`.eve-Card[data-mes="${mesActual}"]`).forEach((card) => (card.style.display = "block"));
+    mesTitulo.textContent = `${nombreMes[mesActual]} 2023`;
+};
+
+/* 
+// Navegar por meses con botones de retroceder y avanzar en la sección de eventos
+
+document.getElementById("btnRetroceder").addEventListener("click", () => {
+    mesActual = (mesActual - 1 + totalMeses) % totalMeses;
+    actualizarTarjetasYTitulo();
+});
+document.getElementById("btnAvanzar").addEventListener("click", () => {
+    mesActual = (mesActual + 1) % totalMeses;
+    actualizarTarjetasYTitulo();
+}); */
+
+// ---- Slide ----
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+let slideInterval;
+
+function changeSlide(step) {
+    slides[index].classList.remove("active");
+    index = (index + step + totalSlides) % totalSlides;
+    slides[index].classList.add("active");
+}
+
+function startSlideShow() {
+    slideInterval = setInterval(() => {
+        changeSlide(1);
+    }, 3000);
+}
+
+function stopSlideShow() {
+    clearInterval(slideInterval);
+}
+
+function reduceSlideInfo() {
+    let activeSlide = document.querySelector(".slide.active");
+    let infoPart = activeSlide.querySelector(".slide-info");
+    infoPart.style.width = "50%";
+}
+
+function resetSlideInfoSize() {
+    let activeSlide = document.querySelector(".slide.active");
+    let infoPart = activeSlide.querySelector(".slide-info");
+    infoPart.style.width = "70%";
+}
+
+const sliderContainer = document.getElementById("slider-container");
+sliderContainer.addEventListener("mouseover", function () {
+    stopSlideShow();
+    reduceSlideInfo();
+});
+sliderContainer.addEventListener("mouseout", function () {
+    startSlideShow();
+    resetSlideInfoSize();
+});
+
+if (slides.length > 0) {
+    slides[index].classList.add("active");
+    startSlideShow();
+}
 
 const desplegarEventos = () => {
     fetch("http://localhost:8080/traereventos", {
