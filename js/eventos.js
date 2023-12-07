7650a57f59d4b45a2739aa8e9dba917ac2b78415
Brandon Fuentes <totisnathor@gmail.com>
2023-12-06 19:04:58 -0600

    Revert "Merge pull request #8 from Totis1/FranciscoMG"
    This reverts commit 3b6dff8da6334a0c1887351b254f79cd1d476b6d, reversing
    changes made to 1cf26df60b18615a9b7c7ed0cc2a9b6cdbe70386.

--- js/eventos.js
+++ js/eventos.js
@@ -3,42 +3,39 @@ const epr = document.getElementById("pr");
 const econtainer = document.getElementById("eContainer");
 const fragment = document.createDocumentFragment();
 
-
 const desplegarEventos = () => {
-
-    fetch('http://localhost:5000/traereventos', {
-        method: 'get'
-    }).then(response => response.json()).then(data => {
-        infoEv = data.data[0]
-        console.log(data.data[0])
-        
-
-
-        data.data.forEach(eve => {
-
-            if (eve.valiacion != "102" ) {
-                return
-             }
-
-            ecard.getElementById('eve-img').setAttribute("src", eve.url_imagen)
-            ecard.getElementById('eve-titulo').textContent = eve.Nombre_eve
-            ecard.getElementById('eve-ponente').textContent = eve.Ponente
-            ecard.getElementById('eve-descripcion').textContent = eve.Descripcion
-
-            const eveInfo = eve.Fecha + "  |  " + eve.Lugar + "  |  " + eve.N_horas + " horas de " + eve.T_horas
-
-            ecard.getElementById('eve-infoEv').textContent = eveInfo
-
-            const clone = ecard.cloneNode(true)
-            fragment.appendChild(clone)
-            econtainer.appendChild(fragment)
-        })
-
-        
+    fetch("http://localhost:5000/traereventos", {
+        method: "get",
     })
-}
+        .then((response) => response.json())
+        .then((data) => {
+            infoEv = data.data[0];
+            console.log(data.data[0]);
 
-window.addEventListener('DOMContentLoaded', () => {
-    desplegarEventos()
-})
+            data.data.forEach((eve) => {
+                ecard.getElementById("eve-titulo").textContent = eve.Nombre_eve;
+                ecard.getElementById("eve-ponente").textContent = eve.Ponente;
+                ecard.getElementById("eve-descripcion").textContent = eve.Descripcion;
 
+                const eveInfo = eve.Fecha + "  |  " + eve.Lugar + "  |  " + eve.N_horas + " horas de " + eve.T_horas;
+
+                ecard.getElementById("eve-infoEv").textContent = eveInfo;
+
+                const clone = ecard.cloneNode(true);
+                fragment.appendChild(clone);
+                econtainer.appendChild(fragment);
+            });
+        });
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+    desplegarEventos();
+
+    const numEvents = 1;
+    econtainer.innerHTML = "";
+    for (let index = 0; index < numEvents; index++) {
+        const clone = ecard.cloneNode(true);
+        fragment.appendChild(clone);
+        econtainer.appendChild(fragment);
+    }
+});
